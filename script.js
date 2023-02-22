7ff63886e5e50a4d340377803db896c9add9750b
Alexis Kuszaj <alexsaleh94@gmail.com>
2023-02-21 22:33:29 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("page loaded...");
+
+var username = document.querySelector("#username");
+
+
+function edit() {
+    username.innerText = "Sonic the Hedgehog";
+}
+
+
+function hide(id){
+    var element = document.getElementById(id);
+    element.remove()
+}
+function hide2(element){
+    element.remove()
+}
